5f4e5524068b2b9720a781f6ad5f1d5375ebbbdd
tmaeder <tmaeder@me.com>
2026-05-14 20:16:06 +0200

    test(resources): drop unused screen import in TopicHubGrid test

--- src/pages/resources/__tests__/TopicHubGrid.test.tsx
+++ src/pages/resources/__tests__/TopicHubGrid.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import { TopicHubGrid } from '../sections/TopicHubGrid';
 import { TOPIC_HUBS } from '../topics.config';
